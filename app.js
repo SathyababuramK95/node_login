d5b09a0543fe8f2e1c99b15441ad368651c01844
Sathyababuram <ksathyababuram95@gmail.com>
2020-12-25 15:43:16 +0530

    final commit

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Credentials', true);
 
     let url = req.originalUrl.split('/')
-    if (url[2] && (url[2] == 'register' || url[2] == 'login')) {
+    if (url[2] && (url[2] == 'register' || url[2] == 'login' || url[2] == 'forgetpassword' || url[2] == 'resetpassword')) {
         next();
     } else {
         let token = req.cookies['access-token'];
